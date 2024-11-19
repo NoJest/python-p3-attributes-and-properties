2766bc84c0bf2dc3c6b4c18b5d7cd3b8c2690ab0
JustinpThomasson <Foreveraendeavour@gmail.com>
2024-11-19 09:42:56 -0500

    this lab had really nitpicky tests

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,33 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self, breed="Mastiff", name="franky"):
+        self._breed = None
+        self._name = None
+        self.breed = breed
+        self.name = name
+        
+        
+    @property
+    def name(self):
+        return self._name
+        
+    @name.setter
+    def name(self, name:str):
+        if not isinstance(name, str):
+            print("Name must be string between 1 and 25 characters.")
+            return
+        if len(name) < 1 or len(name) > 25:
+            print("Name must be string between 1 and 25 characters.")
+            return 
+        self._name = name
+    @property
+    def breed(self):
+        return self._breed
+    
+    @breed.setter
+    def breed(self, breed:str):
+        if breed not in APPROVED_BREEDS:
+            print("Breed must be in list of approved breeds.")
+            return
+        self._breed = breed
--- lib/person.py
+++ lib/person.py
@@ -15,5 +15,36 @@ APPROVED_JOBS = [
     "Purchasing"
 ]
 
+   
 class Person:
-    pass
+    
+    def __init__(self, job="spanking people", name="Guido"):
+        self._job = None
+        self.job = job
+        self.name = name
+    
+    
+    @property
+    def name(self):
+        return self._name
+    @name.setter
+    def name(self, value):
+        if not isinstance(value, str):
+            print("Name must be string between 1 and 25 characters.")
+            return
+        if len(value) < 1 or len(value) > 25:
+            print("Name must be string between 1 and 25 characters.")
+            return
+        self._name = value.title()
+  
+  
+    @property
+    def job(self):
+        return self._job
+    
+    @job.setter
+    def job(self, value):
+        if value not in APPROVED_JOBS:
+            print("Job must be in list of approved jobs.")
+            return 
+        self._job = value
